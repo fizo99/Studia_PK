cfa717a060a8bc15b8693e2c9524ed54335967d1
Krzysztof Huczek <krzysztof.huczek@gmail.com>
2020-10-21 13:01:58 +0200

    lab 2 zad 5 i 6 testy

--- C++/LAB II/5/main.cpp
+++ C++/LAB II/5/main.cpp
@@ -9,6 +9,8 @@
 
 #include <iostream>
 #include <algorithm> //swap
+#include <cassert>
+#include <typeinfo>
 
 using std::cout;
 using std::endl;
@@ -20,17 +22,36 @@ void swap( T & a, T & b ){
     b = temp;
 }
 
-int main(int argc, char *argv[]){
-    int a = 3;
-    int b = 5;
-    cout << "Int: "  << a << " " << b << endl;
-    swap(a,b);
-    cout << "Wynik: " << a << " " << b << endl;
+template < typename T >
+void test( T a, T b ){
+    T aAfter = b;
+    T bAfter = a;
+    std::string type = typeid(T).name();
+    std::string msg = type;
+    msg.append(" test failed");    
 
-    double c = 3.14;
-    double d = 2.78;
-    cout << "Double: "  << c << " " << d << endl;
-    swap(c,d);
-    cout << "Wynik: " << c << " " << d << endl;
-    
+    cout << "Type: " << type << endl;
+    cout << "Before " << a << " " << b << endl;
+    swap(a,b);
+    assert(a == aAfter && b == bAfter && msg.c_str());
+    cout << "After " << aAfter << " " << bAfter << endl <<endl;
+}
+
+
+int main(int argc, char *argv[]){
+    int a = 3, b = 5;
+    test(a,b);
+
+    double c = 3.14, d = 2.718;
+    test(c,d);
+
+    float e = 2.22, f = 13.2;
+    test(e,f);
+
+    char g = 'a',h = 'b';
+    test(g,h);
+
+    int arr[5] = {2,1,3,6,4};
+    int arr2[5] = {5,2,3,6,4};
+    test(arr,arr2);
 }
--- C++/LAB II/6/main.cpp
+++ C++/LAB II/6/main.cpp
@@ -9,28 +9,81 @@
 
 #include <iostream>
 #include <algorithm> //swap
+#include <cassert>
+#include <time.h>
+
+#define size 5
 
 using std::cout;
 using std::endl;
 
-template < typename T >
-void swap( T & a, T & b ){
-    T temp = a;
-    a = b;
-    b = temp;
+template <typename T>
+void swap (T& a, T& b){
+    T c(std::move(a));
+    a=std::move(b);
+    b=std::move(c);
 }
 
-int main(int argc, char *argv[]){
-    int a = 3;
-    int b = 5;
-    cout << "Int: "  << a << " " << b << endl;
-    swap(a,b);
-    cout << "Wynik: " << a << " " << b << endl;
+//works only with same size arrays
+template <typename T, size_t N> 
+void swap (T (&a)[N], T (&b)[N]){
+    for (size_t i = 0; i<N; ++i) 
+        swap (a[i],b[i]);
+}
 
-    double c = 3.14;
-    double d = 2.78;
-    cout << "Double: "  << c << " " << d << endl;
-    swap(c,d);
-    cout << "Wynik: " << c << " " << d << endl;
+template <typename T, size_t N>
+void printArr(T (&arr)[N]){
+    for(int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+template <typename T, size_t N> 
+void test (T (&a)[N], T (&b)[N]){
+    T newA[N];
+    T newB[N];
+    std::copy(std::begin(b), std::end(b), std::begin(newA));
+    std::copy(std::begin(a), std::end(a), std::begin(newB));
+
+    cout << "Before: " << endl;
+    printArr(a);
+    printArr(b);
+
+    swap(a,b);
+    for(int i = 0; i < N; i++)
+        assert(a[i] == newA[i] && b[i] == newB[i] && "test failed");
+
+    cout << "After: " << endl;
+    printArr(a);
+    printArr(b);
+}
+
+
+int main(int argc, char *argv[]){
+    srand(time(NULL));
+
+    int arr1[size] = {2,3,5,6,3};
+    int arr2[size] = {1,2,3,4,5};
+    int arr3[size] = {8,3,1,2,4};
+    int arr4[size] = {3,4,2,1,3};
+
+    double d1[size] = {2.32,3.23,5.22,6.17,3.32};
+    double d2[size] = {1.32,2.67,3.55,4.73,5.0};
+
+    int it = 0;
+
+    cout << "Test " << it << endl;
+    test(arr1,arr2); it++;
+
+    cout << endl << "Test " << it << endl;
+    test(arr3,arr4); it++;
     
+    cout << endl << "Test " << it << endl;
+    test(arr1,arr3); it++;
+
+    cout << endl << "Test " << it << endl;
+    test(arr1,arr4); it++;
+
+    cout << endl << "Test " << it << endl;
+    test(d1,d2); it++;
 }
