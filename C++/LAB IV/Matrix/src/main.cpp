d308aeffafd262730c8397f96df530c3e8e770e0
Krzysztof Huczek <krzysztof.huczek@gmail.com>
2020-11-20 13:18:07 +0100

    cleaner code look

--- C++/LAB IV/Matrix/src/main.cpp
+++ C++/LAB IV/Matrix/src/main.cpp
@@ -1,82 +1,73 @@
 ﻿#include <iomanip>
 #include <iostream>
-#include <stdlib.h> /* srand, rand */
-#include <time.h>   /* time */
 
 #include "Matrix.hpp"
 
 void printOutput(const char *message, char sign, Matrix &m1, Matrix &m2,
                  Matrix &result) {
-  std::cout << message << std::endl << std::endl;
+  printf("%s\n\n", message);
   m1.print();
-  std::cout << sign << std::endl << std::endl;
+  printf("%c\n\n", sign);
   m2.print();
-  std::cout << "= " << std::endl << std::endl;
+  printf("= \n\n");
   result.print();
 }
 
 int main() {
-  srand(time(NULL));
-  int r = rand() % 10 + 4;
-  int c = rand() % 10 + 4;
-
-  Matrix *m_mult1 = new Matrix(r, c);
-  Matrix *m_mult2 = new Matrix(c, r);
-  Matrix *m_square = new Matrix(r);
-  Matrix *m_file = nullptr;
-  Matrix *add = nullptr;
-  Matrix *sub = nullptr;
-  Matrix *mult = nullptr;
-
-  //
+  int r = 8;
+  int c = 5;
 
   ////2 arg constructor
   printf("Matrix(int rows, int cols) rows: %d, cols: %d\n", r, c);
-  m_mult1->print();
-  m_mult1->fillMatrix();
-  m_mult2->fillMatrix();
-  //
-  // 1 arg constructor and set()
+  Matrix matrixOne(r, c);
+  matrixOne.print();
+  // 1 arg constructor
   printf("Matrix(int dim) dim: %d\n", r);
-  m_square->print();
+  Matrix matrixSquare(r);
+  matrixSquare.print();
+
+  // set()
   printf("Matrix filled with set(int n, int m, double val) method:\n");
-  m_square->fillMatrix();
-  m_square->print();
-  int randRow = rand() % r;
-  int randCol = rand() % r;
-  printf("get(int row, int col) row %d , col %d: %lf\n\n", randRow, randCol,
-         m_square->get(randRow, randCol));
+  matrixOne.fillMatrix();
+  matrixOne.print();
+
+  // get()
+  printf("get(int row, int col) row %d , col %d: %lf\n\n", 2, 4,
+         matrixOne.get(2, 4));
 
   // saving to file
-  printf("store(string filename) filename: matrix.txt\n");
-  m_square->print();
-  m_square->store("matrix.txt");
+  printf("Below matrix stored in file matrix.txt\nstore(string filename) "
+         "filename: matrix.txt\n");
+  matrixOne.print();
+  matrixOne.store("matrix.txt");
 
   // constructor with file name
-  printf("Matrix(string filename) filename: \"matrix.txt\"\n");
-  m_file = new Matrix("matrix.txt");
-  m_file->print();
+  printf("Below matrix loaded from file matrix.txt\nMatrix(string filename) "
+         "filename: "
+         "\"matrix.txt\"\n");
+  Matrix matrixFile("matrix.txt");
+  matrixFile.print();
 
   // cols() rows()
-  printf("rows() for the matrix from above return value: %d\n", m_file->rows());
+  printf("rows() for the matrix from above return value: %d\n",
+         matrixFile.rows());
   printf("cols() for the matrix from above return value: %d\n\n",
-         m_file->cols());
+         matrixFile.cols());
 
-  m_file->fillMatrix();
+  Matrix matrixTwo(r, c);
+  Matrix matrixToMult(c, r + 2);
+  matrixTwo.fillMatrix();
+  matrixToMult.fillMatrix();
 
-  add = m_square->add(*m_file);
-  sub = m_square->subtract(*m_file);
-  mult = m_mult1->multiply(*m_mult2);
+  Matrix *addMatrix = matrixOne.add(matrixTwo);
+  Matrix *subMatrix = matrixOne.subtract(matrixTwo);
+  Matrix *multMatrix = matrixOne.multiply(matrixToMult);
 
-  printOutput("ADDITION:", '+', *m_square, *m_file, *add);
-  printOutput("SUBTRACION:", '-', *m_square, *m_file, *sub);
-  printOutput("MULTIPLICATION:", '*', *m_mult1, *m_mult2, *mult);
+  printOutput("ADDITION:", '+', matrixOne, matrixTwo, *addMatrix);
+  printOutput("SUBTRACION:", '-', matrixOne, matrixTwo, *subMatrix);
+  printOutput("MULTIPLICATION:", '*', matrixOne, matrixToMult, *multMatrix);
 
-  delete m_mult1;
-  delete m_mult2;
-  delete m_square;
-  delete m_file;
-  delete add;
-  delete sub;
-  delete mult;
+  delete addMatrix;
+  delete subMatrix;
+  delete multMatrix;
 }
